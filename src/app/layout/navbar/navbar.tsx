9d34c6ad36c1905babc7c82192bfafe9411dbd1c
vladburthev <burthev04@gmail.com>
2025-01-20 23:30:43 +0200

    navbar

--- src/app/layout/navbar/navbar.tsx
+++ src/app/layout/navbar/navbar.tsx
@@ -1,10 +1,28 @@
 import type { FC } from "react";
 import styles from "../styles.module.scss";
+import { Link } from "react-router-dom";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
-  return <nav className={styles.navbar}>navbar</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles["navbar-container"]}>
+        <div className={styles["navbar-title"]}>
+          <Link className={styles["navbar-title__link"]} to={"/"}>
+            ST Dashboard
+          </Link>
+        </div>
+        <ul className={styles["navbar-list"]}>
+          <li className={styles["navbar-list__item"]}>
+            <Link className={styles["navbar-list__link"]} to={"/customers"}>
+              Customers
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
